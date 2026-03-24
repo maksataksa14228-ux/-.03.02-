1ca0a3610a138e04bdb82f0995566a6a7d5cbfa3
SIN <maksim_andreev3011@mail.ru>
2026-03-24 19:19:10 +0300

    Add error handling Closes #3

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 def main():
-    print("Hello from my project!")
-
+    try:
+        print("Hello from my project!")
+        print("version 0.1.0")
+    except Exception as e:
+        print(f"Error: {e}")
 if __name__ == "__main__":
     main()
